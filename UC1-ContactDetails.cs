16c15ea5176753806d66d7954ba20cfdcb63a27f
Darsh <darshankondhare2698@gmail.com>
2022-10-16 15:16:23 +0530

    ContactDetails Program Added

---
+++ UC1-ContactDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystems
+{
+    public class UC1_ContactDetails
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PhoneNumber;
+        public string Email;
+
+
+    }
+}
+
