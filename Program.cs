bd99258f7a27415494ff91e30617c71b1707d762
Darsh <darshankondhare2698@gmail.com>
2022-10-16 19:07:02 +0530

    UC4_DeleteDetails

--- Program.cs
+++ Program.cs
@@ -9,10 +9,14 @@
            UC2_AddDetails addDetails = new UC2_AddDetails();
            addDetails.EnterInput();
            addDetails.ViewTheDetails();
-            //addDetails.PrintTheDetails();
+           //addDetails.PrintTheDetails();
 
             UC3_EditDetails editDetails = new UC3_EditDetails();
             editDetails.EditUsingName();
+
+            UC4_DeleteDetails deleteDetails = new UC4_DeleteDetails();
+            deleteDetails.DeletetheName();
+
         }
     }
 }
